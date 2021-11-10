b3da0ff8c36a2a0a4064586c7e534807966518bb
Zax27 <zzax927@gmail.com>
2021-11-10 13:31:55 -0800

    Getting things started

---
+++ commands/getSchedule.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'Get Schedule',
+    description: 'Retrieve a schedule',
+    execute(message, args) {
+        if (!args.length) {
+            return message.channel.send(`Please provide more arguments, ${message.author}`);
+        }
+        else if (args.length === 1) {
+
+        }
+        else if (args.length === 2) {
+
+        }
+        else {
+            return message.channel.send('Too many arguments');
+        }
+    },
+};
